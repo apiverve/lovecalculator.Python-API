31ef8e0baccca25351b0166400c315d1937d79ce
APIVerve Team <hello@apiverve.com>
2025-03-28 17:32:39 -0500

    Bug Fixes

--- src/apiverve_lovecalculator/apiClient.py
+++ src/apiverve_lovecalculator/apiClient.py
@@ -10,10 +10,10 @@ class LovecalculatorAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/lovecalculator'
+        self.base_url = 'https://api.apiverve.com/v1/lovecalculator'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
